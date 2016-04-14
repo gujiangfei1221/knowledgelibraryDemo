6f841a6cce7ed13d69dd23e29a31b9ce651f8cac
gujiangfei1221 <929008776@qq.com>
2016-04-14 17:21:15 +0800

    1

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,50 +1,68 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 /**
  * Created by PhpStorm.
  * User: gujiangfei
  * Date: 16/4/11
  * Time: 20:15
  */
-
-class Main extends CI_Controller{
+class Main extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Mainmodel');
     }
 
-    public function index(){
-        if(!isset($_SESSION['name'])){
+    public function index()
+    {
+        if (!isset($_SESSION['name'])) {
             echo '<script>alert("请登录系统!")</script>';
-            echo '<script>window.location.href=\''.site_url('Login/index').'\';</script>';
+            echo '<script>window.location.href=\'' . site_url('Login/index') . '\';</script>';
             return;
         }
         $data = $this->Mainmodel->selectlanmu();
         $data2 = $this->getmulu($data);
 
-        $tmp = json_encode($data2,JSON_UNESCAPED_UNICODE);
-        $tmp = str_replace('parentname','text',$tmp);
-        $tmp = str_replace('name','text',$tmp);
-        $tmp = str_replace('child','nodes',$tmp);
+        var_dump($this->handledata($data2));
+
+        $tmp = json_encode($data2, JSON_UNESCAPED_UNICODE);
+        $tmp = str_replace('parentname', 'href', $tmp);
+        $tmp = str_replace('name', 'text', $tmp);
+        $tmp = str_replace('child', 'nodes', $tmp);
         $data3['lanmu'] = $tmp;
-        $this->load->view('mainview',$data3);
+        $this->load->view('mainview', $data3);
     }
 
-    public function getmulu($data,$parentname = 'root'){
+    public function getmulu($data, $parentname = 'root')
+    {
         $tree = array();
-        foreach($data as $row){
-            if($row['parentname']==$parentname){
-                $tmp = $this->getmulu($data,$row['name']);
-                if($tmp){
-                    $row['child']=$tmp;
+        foreach ($data as $row) {
+            if ($row['parentname'] == $parentname) {
+                $tmp = $this->getmulu($data, $row['name']);
+                if ($tmp) {
+                    $row['child'] = $tmp;
                 }
-                $tree[]=$row;
+                $tree[] = $row;
             }
         }
         return $tree;
     }
 
+    public function handledata($data)
+    {
+        $arr = array();
+        foreach ($data as $row) {
+            $row['parentname'] = $row['name'];
+            if (isset($row['child'])) {
+                $this->handledata($row['child']);
+            }
+            $arr[] = $row;
+            var_dump($arr);
+        }
+        return $arr;
+    }
 }
 
 ?>
