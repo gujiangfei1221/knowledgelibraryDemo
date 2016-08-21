108fb37b9d9167c63d6afc998bc74f642b470805
gujiangfei1221 <929008776@qq.com>
2016-08-21 08:31:03 +0800

    1

--- application/controllers/Moniter.php
+++ application/controllers/Moniter.php
@@ -16,16 +16,8 @@ class Moniter extends CI_Controller{
     }
 
     public function index(){
-        if(!isset($_SESSION['name'])){
-            echo '<script>alert("请登录系统!")</script>';
-            echo '<script>window.location.href=\''.site_url('Login/index').'\';</script>';
-            return;
-        }
-        if($_SESSION['duiwai'] == 'yes'){
-            echo '<script>alert("您没有权限访问该模块!")</script>';
-            echo '<script>window.location.href=\''.site_url('Select/index').'\';</script>';
-            return;
-        }
+        $data = $this->uri->segment(2);
+        var_dump($data);
         $this->load->view('moniterview');
     }
 }
--- application/views/moniterview.php
+++ application/views/moniterview.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <title>服务器监控</title>
-
+    <script src="<?= $this->config->item('base_url'); ?>/js/security.js"></script>
     <!-- Bootstrap -->
     <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <script src="//cdn.bootcss.com/echarts/3.2.2/echarts.min.js"></script>
@@ -21,78 +21,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <![endif]-->
 </head>
 <body>
-    <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
-    <div id="main" style="width: 800px;height:400px;"></div>
-    <script type="text/javascript">
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('main'));
+    <script>
+        function test(){
 
-        // 指定图表的配置项和数据
-        option = {
-            title: {
-                text: '折线图堆叠'
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
-            },
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            toolbox: {
-                feature: {
-                    saveAsImage: {}
-                }
-            },
-            xAxis: {
-                type: 'category',
-                boundaryGap: false,
-                data: ['周一','周二','周三','周四','周五','周六','周日']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    name:'邮件营销',
-                    type:'line',
-                    stack: '总量',
-                    data:[120, 132, 101, 134, 90, 230, 210]
-                },
-                {
-                    name:'联盟广告',
-                    type:'line',
-                    stack: '总量',
-                    data:[220, 182, 191, 234, 290, 330, 310]
-                },
-                {
-                    name:'视频广告',
-                    type:'line',
-                    stack: '总量',
-                    data:[150, 232, 201, 154, 190, 330, 410]
-                },
-                {
-                    name:'直接访问',
-                    type:'line',
-                    stack: '总量',
-                    data:[320, 332, 301, 334, 390, 330, 320]
-                },
-                {
-                    name:'搜索引擎',
-                    type:'line',
-                    stack: '总量',
-                    data:[820, 932, 901, 934, 1290, 1330, 1320]
-                }
-            ]
-        };
-
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+        }
     </script>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
