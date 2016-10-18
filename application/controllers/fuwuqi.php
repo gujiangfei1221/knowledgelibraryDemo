3f15b0ef94dea87fec977bf86fc9b7c17b5681eb
gujiangfei1221 <929008776@qq.com>
2016-10-18 17:13:31 +0800

    1

---
+++ application/controllers/fuwuqi.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: gujiangfei
+ * Date: 16/4/11
+ * Time: 20:14
+ */
+
+class Fuwuqi extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public function index(){
+        if(!isset($_SESSION['name'])){
+            echo '<script>alert("请登录系统!")</script>';
+            echo '<script>window.location.href=\''.site_url('Login/index').'\';</script>';
+            return;
+        }
+
+        $this->load->view('fuwuqiview');
+    }
+}
+
+?>
